df4858d088b5c1dca1852689c008ebaa5cc1fa69
parkhj <rolco2@hanmail.net>
2023-07-27 13:34:50 +0900

    python_operator_업로드

--- dags/dags_pyhon_import_func.py
+++ dags/dags_pyhon_import_func.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 import pendulum
-import datetime
+#import datetime
 from airflow.operators.python import PythonOperator
 from common.common_func import get_sftp
 
@@ -8,10 +8,10 @@ with DAG(
     dag_id = "dags_python_import_func",
     schedule = "30 6 * * *",
     start_date = pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=false
+    catchup=False
 ) as dag:
     
     task_get_sftp = PythonOperator(
         task_id = 'task_get_sftp',
-        python_callable= = get_sftp
+        python_callable= get_sftp
     )
