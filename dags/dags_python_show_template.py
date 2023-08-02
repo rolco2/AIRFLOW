60e84b8224d1469b169d950eb277ac5444b7370a
parkhj <rolco2@hanmail.net>
2023-08-02 10:58:37 +0900

    bash_with_template_어로드_코드수정2

--- dags/dags_python_show_template.py
+++ dags/dags_python_show_template.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 with DAG(
     dag_id= 'dags_python_show_template',
     schedule = "30 9 * * *",
-    start_date = pendulum.datetime(2023, 3, 1, tz = "Asia/Seoul"),
+    start_date = pendulum.datetime(2023, 7, 1, tz = "Asia/Seoul"),
     catchup = True
 ) as dag:
 
