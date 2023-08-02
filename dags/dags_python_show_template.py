70d1c8326409bb53b45b89c02ad64e94887fb4fb
parkhj <rolco2@hanmail.net>
2023-08-02 10:37:08 +0900

    python_template_ex_update

---
+++ dags/dags_python_show_template.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+#import datetime
+from airflow.decorators import task
+
+with DAG(
+    dag_id= 'dags_python_show_template'
+    schedule = "30 9 * * *",
+    start_date = pendulum.datetime(2023, 3, 1, tz = "Asia/Seoul"),
+    catchup = TRUE
+) as dag:
+
+    @task(task_id = 'python_task')
+    def show_tempates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+
+    show_tempates()
