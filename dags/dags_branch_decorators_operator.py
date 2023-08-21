962578071baf036e3175cd84a00081abfb265f11
parkhj <rolco2@hanmail.net>
2023-08-21 10:49:35 +0900

    error_update_ver1

--- dags/dags_branch_decorators_operator.py
+++ dags/dags_branch_decorators_operator.py
@@ -12,37 +12,37 @@ with DAG(
     catchup=False   
 ) as dag:
 
-    @task.branch(task_id = 'python_branch_task')
-    def select_random():
-        import random
-
-        item_lst = ['A','B','C']
-        selected_item = random.choice(item_lst)
-        if selected_item == 'A' :
-            return 'task_a'
-        elif selected_item == 'B' :
-            return ['task_b','task_c']
-
-
-    def common_func(**kwargs):
-        print(kwargs['selected'])
-        
-    task_a = PythonOperator(
-        task_id = 'task_a',
-        python_callable=common_func,
-        op_kwargs={'selected':'A'}    
-    )
-    
-    task_b = PythonOperator(
-        task_id = 'task_b',
-        python_callable=common_func,
-        op_kwargs={'selected':'B'}    
-    )
-    
-    task_c = PythonOperator(
-        task_id = 'task_c',
-        python_callable=common_func,
-        op_kwargs={'selected':'C'}    
-    )
-
-    select_random() >> [task_a, task_b, task_c]
+#    @task.branch(task_id = 'python_branch_task')
+#    def select_random():
+#        import random
+#
+#        item_lst = ['A','B','C']
+#        selected_item = random.choice(item_lst)
+#        if selected_item == 'A' :
+#            return 'task_a'
+#        elif selected_item == 'B' :
+#            return ['task_b','task_c']
+#
+#
+#    def common_func(**kwargs):
+#        print(kwargs['selected'])
+#        
+#    task_a = PythonOperator(
+#        task_id = 'task_a',
+#        python_callable=common_func,
+#        op_kwargs={'selected':'A'}    
+#    )
+#    
+#    task_b = PythonOperator(
+#        task_id = 'task_b',
+#        python_callable=common_func,
+#        op_kwargs={'selected':'B'}    
+#    )
+#    
+#    task_c = PythonOperator(
+#        task_id = 'task_c',
+#        python_callable=common_func,
+#        op_kwargs={'selected':'C'}    
+#    )
+#
+#    select_random() >> [task_a, task_b, task_c]
--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -13,41 +13,41 @@ with DAG(
     catchup=False   
 ) as dag:
     
-    def select_random() :
-        import random
-
-        item_lst = ['A','B','C']
-        selected_item = random.choice(item_lst)
-        if selected_item == 'A' :
-            return 'task_a'
-        elif selected_item == 'B' :
-            return ['task_b','task_c']
-            
-    python_branch_task = BranchPythonOperator(
-        task_id = 'python_branch_task',
-        python_callable = select_random
-    
-    )
-    
-    def common_func(**kwargs):
-        print(kwargs['selected'])
-        
-    task_a = PythonOperator(
-        task_id = 'task_a',
-        python_callable=common_func,
-        op_kwargs={'selected':'A'}    
-    )
-    
-    task_b = PythonOperator(
-        task_id = 'task_b',
-        python_callable=common_func,
-        op_kwargs={'selected':'B'}    
-    )
-    
-    task_c = PythonOperator(
-        task_id = 'task_c',
-        python_callable=common_func,
-        op_kwargs={'selected':'C'}    
-    )
-
-    python_branch_task >> [task_a, task_b, task_c]
+#    def select_random() :
+#        import random
+#
+#        item_lst = ['A','B','C']
+#        selected_item = random.choice(item_lst)
+#        if selected_item == 'A' :
+#            return 'task_a'
+#        elif selected_item == 'B' :
+#            return ['task_b','task_c']
+#            
+#    python_branch_task = BranchPythonOperator(
+#        task_id = 'python_branch_task',
+#        python_callable = select_random
+#    
+#    )
+#    
+#    def common_func(**kwargs):
+#        print(kwargs['selected'])
+#        
+#    task_a = PythonOperator(
+#        task_id = 'task_a',
+#        python_callable=common_func,
+#        op_kwargs={'selected':'A'}    
+#    )
+#    
+#    task_b = PythonOperator(
+#        task_id = 'task_b',
+#        python_callable=common_func,
+#        op_kwargs={'selected':'B'}    
+#    )
+#    
+#    task_c = PythonOperator(
+#        task_id = 'task_c',
+#        python_callable=common_func,
+#        op_kwargs={'selected':'C'}    
+#    )
+#
+#    python_branch_task >> [task_a, task_b, task_c]
