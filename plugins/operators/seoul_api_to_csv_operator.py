f436e4b872eab806f5d3edb9dd4b360f496cecf8
parkhj <rolco2@hanmail.net>
2023-09-05 13:26:27 +0900

    aaa

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -39,6 +39,8 @@ class SeoulApitoCsvOperator(BaseOperator):
         if not os.path.exists(self.path):
             os.system(f'mkdir -p {self.path}')
         
+        print(self.path)
+
         total_row_df.to_csv(self.path + '/' + self.file_name, encoding = 'utf-8', index=False)
 
     def _call_api(self, base_url, start_row, end_row):
